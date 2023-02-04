ff1592dd0c9457262c48755b96a1230bce58f5e7
Morgane Charreteur <morganecharreteur@gmail.com>
2023-02-04 15:46:36 +0100

    Starter

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { Client } from "@notionhq/client";
+
+const config = useRuntimeConfig();
+const notion = new Client({ auth: config.notionApiKey });
+
+(async () => {
+  const databaseId = "320f54753392429280755f8049f6fc68";
+  const response = await notion.databases.retrieve({ database_id: databaseId });
+  console.log(response);
+})();
+</script>
+
+<template>
+  <div>Home</div>
+</template>
